148b5675ba6c859689d9cf7f31c794e4f002ec7e
wagnerelvio <wagnerelvio@gmail.com>
2025-05-16 22:52:02 -0300

    Create code-06.java

---
+++ codigos16052/code-06.java
@@ -0,0 +1,10 @@
+// Strings em Java
+public class ExemploString {
+    public static void main(String[] args) {
+        String nome = "Java";
+
+        System.out.println("Tamanho: " + nome.length());
+        System.out.println("Maiúsculas: " + nome.toUpperCase());
+        System.out.println("Contém 'va'? " + nome.contains("va"));
+    }
+}
