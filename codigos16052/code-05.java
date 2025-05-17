5486e7c75c99ed9c93e7e18fefff66858fdef0b7
wagnerelvio <wagnerelvio@gmail.com>
2025-05-16 22:51:25 -0300

    Create code-05.java

---
+++ codigos16052/code-05.java
@@ -0,0 +1,10 @@
+// Arrays em Java
+public class ExemploArray {
+    public static void main(String[] args) {
+        int[] notas = {7, 8, 6, 10};
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println("Nota " + i + ": " + notas[i]);
+        }
+    }
+}
